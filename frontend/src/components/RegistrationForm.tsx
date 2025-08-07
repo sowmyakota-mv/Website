61d4528b5136ab0609b1d4ce4511fd105a2980cb
sowmyakota-mv <mastersvisa.da31@gmail.com>
2025-08-07 19:08:24 +0530

    Update RegistrationForm.tsx

--- frontend/src/components/RegistrationForm.tsx
+++ frontend/src/components/RegistrationForm.tsx
@@ -53,7 +53,7 @@ function RegistrationForm() {
 
     try {
       const response = await fetch(
-        "https://backend-website-pkm7.onrender.com/api/register",
+        "https://websitebackend-8a3q.onrender.com/api/register",
         {
           method: "POST",
           headers: {
