5d6c33b7bdd9db5c44746456a2d546cc54b9cb11
sowmyakota-mv <mastersvisa.da31@gmail.com>
2025-08-07 19:00:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 // Allowed origins
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://websites-qjlp.onrender.com'
+  'https://websitefrontend-7s05.onrender.com/'
 ];
 
 app.use(cors({
